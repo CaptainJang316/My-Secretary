71f3face552f4eb6c3d9aa20253229cd517e55c0
unknown <gksrkfkaryghl@gmail.com>
2022-12-21 23:59:35 +0900

    build server using Express

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const api = require('./src/index');
-app.use('/api', api);
+// const api = require('./src/index');
+// app.use('/api', api);
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send({ test : 'this is test!!'});
-})
+// app.get('/', (req, res) => {
+//     res.send({ test : 'this is test!!'});
+// })
+
+app.use( express.static( path.join(__dirname, '../build') ) );
+
+app.get('*',function(request, response){
+    response.sendFile( path.join(__dirname, '../build/index.html'))
+  })
 
 app.listen(PORT, () => {
     console.log(`Server run : http://localhost:${PORT}/`)
